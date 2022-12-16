c33081106864ed7b19b39f48e2517446c5b73803
ReDay Zarra <redayzarra@gmail.com>
2022-12-15 21:45:28 -0500

    Adding notes on variables

---
+++ JS-Basics/variables.js
@@ -0,0 +1,10 @@
+let name; //This variable will be undefined
+let me = 'ReDay'; //The string is stored in me
+
+// Variables cannot be reserved keywords, you will get an error. 
+// Variables should be named meaningfully.\
+// Variables cannot start with a number list 5two
+// Variables cannot have a space or a hyphen
+
+console.log(name);
+console.log(me);
