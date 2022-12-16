792830633ebe66adaab701d6ec94cf317a8cfba4
ReDay Zarra <redayzarra@gmail.com>
2022-12-15 21:52:00 -0500

    Adding notes on primitives

---
+++ JS-Basics/primitives.js
@@ -0,0 +1,5 @@
+let name = 'ReDay'; // This is called a string literal
+let age = 21; // This is called a number literal
+let isCool = false; // This is called a boolean literal
+
+let myName = null; // This clears a value of the variable, basically an empty
