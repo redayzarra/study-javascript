cbbc0da4eed91f0aaf6fcc2ca0c6863a096efca4
ReDay Zarra <redayzarra@gmail.com>
2022-12-15 22:31:24 -0500

    Adding notes on function basics

---
+++ JS-Basics/functions.js
@@ -0,0 +1,19 @@
+function myFunction() { // This initiates the function 
+  console.log('Another JS course');
+}
+
+myFunction(); // Calls our function
+
+
+function whatsUp (name) {
+  console.log("What's up, " + name +"!");
+}
+
+whatsUp('ReDay'); // Passing in my name as an argument to our function
+
+
+function yetAgain(firstName, lastName) {
+  console.log("Good god... please not again, " + firstName + " " + lastName + "!")
+}
+
+yetAgain('Adam', 'Sandler');
