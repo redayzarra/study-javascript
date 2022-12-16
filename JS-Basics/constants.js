e3c7766cdea800eec7256af3af5f4dfa8cecc5b8
ReDay Zarra <redayzarra@gmail.com>
2022-12-15 21:48:14 -0500

    Adding notes on constants

---
+++ JS-Basics/constants.js
@@ -0,0 +1,6 @@
+// Certain values should be fixed and cannot be changed
+
+const interestRate = 0.3; // Makes it so that the variable cannot be reassigned
+console.log(interestRate);
+
+// const should be the default choice unless we need to change variables
