e383171e8ccb746c97b996e68b0c968e8104cffd
ReDay Zarra <redayzarra@gmail.com>
2022-12-15 22:38:52 -0500

    Finishing course with function types

--- JS-Basics/functions.js
+++ JS-Basics/functions.js
@@ -11,9 +11,19 @@ function whatsUp (name) {
 
 whatsUp('ReDay'); // Passing in my name as an argument to our function
 
-
+// Performs a task
 function yetAgain(firstName, lastName) {
   console.log("Good god... please not again, " + firstName + " " + lastName + "!")
 }
 
 yetAgain('Adam', 'Sandler');
+
+//Calculates a value
+function square(number) {
+  return number * number; // Think of return as the value of the function
+}
+
+let ourNumber = square(2); // Assigns the return value with argument to variable
+console.log(ourNumber); 
+
+console.log(square(3)); // Or just directly print the result of the function
